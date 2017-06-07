818d5420aafe05edb98aaae861b6c71f009c0870
Rafik Farhad <rafikfarhad@gmail.com>
2017-06-07 12:41:17 +0600

    07 June 2017

---
+++ app/src/main/java/io/github/rafikfarhad/ssamultitask/SendMessagePageActivity.java
@@ -0,0 +1,24 @@
+package io.github.rafikfarhad.ssamultitask;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SendMessagePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message_page);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+//        TextView textView = (TextView) findViewById(R.id.mytextview);
+//        textView.setText(message);
+
+    }
+}
